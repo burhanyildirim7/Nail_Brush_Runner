a04b68b53d58f50b440dbce549dc3dc991cc0dc2
Kuntay KOCA <kuntay.koca.development@gmail.com>
2022-03-30 06:38:03 +0300

    kalsin böyle

--- Assets/Scripts/TelScript.cs
+++ Assets/Scripts/TelScript.cs
@@ -24,40 +24,76 @@ public class TelScript : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (FircaCogalmaScripti.instance._durum == true &&_myId !=0)
+        {
+            transform.eulerAngles = new Vector3(-90 + (_myId * 3),90,-180);
+        }
+        else if(FircaCogalmaScripti.instance._durum == false && _myId != 0)
+        {
+            transform.eulerAngles = _idleRotation;
+        }
+        else
+        {
+                
+        }
+
+        /*
         if (_myId == 0)
         {
 
         }
         else
         {
-            if (FircaCogalmaScripti.instance._durum == true && transform.eulerAngles != new Vector3(-90 + (_myId * 4), 90, -180))
+            if (_myId<0)
             {
-                //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(-90 + (_myId * 4), 90, -180), 1f);
-                StartCoroutine(AciArtirma());
-            }
-            else if (FircaCogalmaScripti.instance._durum == false && transform.eulerAngles != _idleRotation)
-            {
-                //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, _idleRotation, 1f);
-                StartCoroutine(AciAzaltma());
+                if (FircaCogalmaScripti.instance._durum == true && transform.eulerAngles.x <= (-90 + (_myId * 4)))
+                {
+                    //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(-90 + (_myId * 4), 90, -180), 1f);
+                    StartCoroutine(AciArtirma());
+                }
+                else if (FircaCogalmaScripti.instance._durum == false && transform.eulerAngles.x >= _idleRotation.x)
+                {
+                    //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, _idleRotation, 1f);
+                    StartCoroutine(AciAzaltma());
+                }
+                else
+                {
+
+                }
+
             }
             else
             {
+                if (FircaCogalmaScripti.instance._durum == true && transform.eulerAngles.x >= (-90 + (_myId * 4)))
+                {
+                    //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(-90 + (_myId * 4), 90, -180), 1f);
+                    StartCoroutine(AciArtirma());
+                }
+                else if (FircaCogalmaScripti.instance._durum == false && transform.eulerAngles.x <= _idleRotation.x)
+                {
+                    //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, _idleRotation, 1f);
+                    StartCoroutine(AciAzaltma());
+                }
+                else
+                {
+
+                }
 
             }
         }
 
         IEnumerator AciArtirma()
         {
-            transform.eulerAngles = transform.eulerAngles + new Vector3(-90f + (_myId * 4 * 0.01f), 90, -180);
+            transform.eulerAngles = transform.eulerAngles - new Vector3((_myId * 4 * 0.01f), 0, 0);
             yield return new WaitForSeconds(0.01f);
         }
 
 
         IEnumerator AciAzaltma()
         {
-            transform.eulerAngles = transform.eulerAngles - new Vector3(-90f + (_myId * 4 * 0.01f), 90, -180);
+            transform.eulerAngles = transform.eulerAngles + new Vector3((_myId * 4 * 0.01f), 0, 0);
             yield return new WaitForSeconds(0.01f);
         }
-
+        */
     }
 }
